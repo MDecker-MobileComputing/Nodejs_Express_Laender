39f850394c78c8d9b0b2739d4334248cc18e93c7
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-03-22 16:16:44 +0100

    Morgan-HTTP-Logger dazu

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
-
+import fs from "fs";
 
 const app = new express();
 
-app.use(express.json());
+// Log-Datei für HTTP-Zugriffe
+const accessLogStream = fs.createWriteStream("access.log", { flags: "a" })
+app.use(morgan("combined", { stream: accessLogStream })); // combined: Standard-Format von Apache Web-Server
+
+const errorLogStream = fs.createWriteStream("access-error.log", { flags: "a" });
+app.use(morgan("combined", {
+  stream: errorLogStream,
+  skip: function (req, res) { return res.statusCode < 400 }
+}));
+
+app.use( express.json() );
 
 const PORT_NUMMER = 8080;
 app.listen(PORT_NUMMER, () => {
