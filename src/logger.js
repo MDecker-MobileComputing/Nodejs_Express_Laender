0050a827bab792ed512b6a64f6bc9d94efb6b1f0
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-03-22 16:54:48 +0100

    neu: logger.js mit Winston- und Morgan-Logger

---
+++ src/logger.js
@@ -0,0 +1,63 @@
+import fs      from "fs";
+import path    from "path";
+import morgan  from "morgan";
+import url     from 'url';
+import winston from "winston";
+
+/** Ordner relativ zum Wurzelverzeichnis, in den die Log-Dateien geschrieben werden. */
+const LOG_ORDNER = "logs/";
+
+
+/**
+ * Erzeugt einen Logger mit Winston, der in einer bestimmten Quellcode-Datei zu verwenden ist.
+ * <br><br>
+ *
+ * Log-Levels nach absteigender Prio: error, warn, info, http, verbose, debug, silly
+ */
+export function getLogger(importMetaUrl) {
+
+  const dateiname = path.basename(url.fileURLToPath(importMetaUrl));
+
+  // Ziele für die Log-Nachrichten definieren; wenn nicht in Produktionsumgebung, dann zusätzlich auf Konsole ausgeben
+  const transportsArray =  [
+    new winston.transports.File({ filename: `${LOG_ORDNER}/application-error.log`, level: "error" }),
+    new winston.transports.File({ filename: `${LOG_ORDNER}/application.log` }),
+  ]
+  if (process.env.NODE_ENV !== "production") {
+    transportsArray.push(new winston.transports.Console());
+  }
+
+  return winston.createLogger({
+    level: "info", // kleinste Log-Prio (Log-Level), die noch protokolliert wird
+    format: winston.format.combine(
+      winston.format.label({ label: dateiname }),
+      winston.format.timestamp({ format: "YYYY-MM-DD_HH:mm:ss:SSS" }),
+      winston.format.printf(nachricht => {
+        return `${nachricht.timestamp} [${nachricht.label}] ${nachricht.level.toUpperCase()}: ${nachricht.message}`;
+      })
+    ),
+    defaultMeta: { service: "laender" },
+    transports: transportsArray
+  });
+}
+
+
+/**
+ * Middleware-Funktion von Library "morgan", die HTTP-Zugriffe protokolliert,
+ * registriert für Express-App.
+ *
+ * @param {*} app Express-Objekt
+ */
+export function registriereHttpLogger(app) {
+
+    const MODE_APPEND = { flags: "a" };
+
+    const accessLogStream = fs.createWriteStream(`${LOG_ORDNER}/access.log`, MODE_APPEND)
+    app.use(morgan("combined", { stream: accessLogStream })); // combined: Standard-Format von Apache Web-Server
+
+    const errorLogStream = fs.createWriteStream(`${LOG_ORDNER}/access-error.log`, MODE_APPEND);
+    app.use(morgan("combined", {
+        stream: errorLogStream,
+        skip: function (req, res) { return res.statusCode < 400 }
+    }));
+}
--- src/main.js
+++ src/main.js
@@ -1,23 +1,17 @@
 import express from "express";
-import morgan from "morgan";
 
-import fs from "fs";
+import { getLogger } from './logger.js';
+
+const logger = getLogger(import.meta.url);
 
 const app = new express();
 
 // Log-Datei für HTTP-Zugriffe
-const accessLogStream = fs.createWriteStream("access.log", { flags: "a" })
-app.use(morgan("combined", { stream: accessLogStream })); // combined: Standard-Format von Apache Web-Server
 
-const errorLogStream = fs.createWriteStream("access-error.log", { flags: "a" });
-app.use(morgan("combined", {
-  stream: errorLogStream,
-  skip: function (req, res) { return res.statusCode < 400 }
-}));
 
 app.use( express.json() );
 
 const PORT_NUMMER = 8080;
 app.listen(PORT_NUMMER, () => {
-    console.log(`Web-Server lauscht auf Port ${PORT_NUMMER}.\n`);
+    logger.info(`Web-Server gestartet auf Port ${PORT_NUMMER}.\n`);
 });
